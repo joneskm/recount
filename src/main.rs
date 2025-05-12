40169b370ecc6c548aad6adc405342b8a66ea4ef
Kevin Jones <kevin.jonesat@gmail.com>
2025-05-12 15:52:54 +0100

    Add command line args
    Add a command to pass a filename which will be parsed.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,26 @@
-use recount::tokenizer::Tokenizer;
+use clap::Parser;
+use std::fs::read_to_string;
+use std::path::PathBuf;
+
+use recount::{parser::parse, tokenizer::Tokenizer};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// The Beancount input filename to load
+    #[arg(short, long, value_name = "FILE")]
+    file: PathBuf,
+}
 
 fn main() {
-    let mut tokenizer = Tokenizer::new("2023-02-01 1.2334343");
-    // let _token = tokenizer.next_token();
+    let cli = Cli::parse();
+
+    let buffer = read_to_string(cli.file).unwrap();
+    let tokenizer = Tokenizer::new(buffer);
+    let accounts_doc = parse(tokenizer).unwrap();
+
+    for (account, balance) in accounts_doc.balances() {
+        println!("{:?}", account);
+        println!("{:?}", balance);
+    }
 }
