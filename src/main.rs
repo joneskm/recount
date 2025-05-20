797c8a51e386ff759c681f84cfcd8dd92aabec0c
Kevin Jones <kevin.jonesat@gmail.com>
2025-05-20 16:07:49 +0100

    Handle errors correctly in main
    Remove unwraps in main, write error messages and return exit codes.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use clap::Parser;
-use std::fs::read_to_string;
+use std::env::args;
 use std::path::PathBuf;
+use std::{fs::read_to_string, process::ExitCode};
 
 use recount::{parser::parse, tokenizer::Tokenizer};
+
+const CRATE_NAME: &str = env!("CARGO_PKG_NAME");
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -12,15 +15,30 @@ struct Cli {
     file: PathBuf,
 }
 
-fn main() {
+fn main() -> ExitCode {
+    match run() {
+        Ok(_) => ExitCode::SUCCESS,
+        Err(e) => {
+            let arg0 = args().next().unwrap_or(CRATE_NAME.to_string());
+            eprintln!("{}: {}", arg0, e);
+            ExitCode::FAILURE
+        }
+    }
+}
+
+fn run() -> Result<(), String> {
     let cli = Cli::parse();
 
-    let buffer = read_to_string(cli.file).unwrap();
+    let buffer = read_to_string(&cli.file)
+        .map_err(|e| format!("cannot access '{}': {}", cli.file.display(), e))?;
+
     let tokenizer = Tokenizer::new(buffer);
-    let accounts_doc = parse(tokenizer).unwrap();
+    let accounts_doc = parse(tokenizer).map_err(|e| format!("parsing error: {}", e))?;
 
     for (account, balance) in accounts_doc.balances() {
         println!("{:?}", account);
         println!("{:?}", balance);
     }
+
+    Ok(())
 }
